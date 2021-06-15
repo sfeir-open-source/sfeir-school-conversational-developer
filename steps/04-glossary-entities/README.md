f22eacff0a5d117e4e169d7027c6cc66a69a3c4c
Fabien Bonalair <bonalair.f@sfeir.com>
2021-06-15 16:41:51 +0200

    WIP Adding exercice 04

---
+++ steps/04-glossary-entities/README.md
@@ -0,0 +1,14 @@
+# To Do
+Let's step up the dynamic answer with element from Dialogflow's request: entities and parameter extraction.
+
+# Tips
+Dialogflow's recognised parameter is present in the request but be careful, it is NOT required for this intent.   
+Definitions are present in `00-ressources/definitions.js`, but it does not contain all possible cases (on purpose).
+
+Lost connection? Debug using intent `_ping`.
+
+Reminder of intent activation:
+- Launch Ngrok and local serveur
+- Activate fulfillment in Dialogflow interface Dialogflow
+- Connect Dialogflow to local serveur
+- Verify connexion
---
+++ steps/04-glossary-entities/index.js
@@ -0,0 +1,28 @@
+const simpleResponse = require('../00-ressources/simple-text-response.json');
+const authors = require('../package.json').authors.join(',');
+const BOT_AGE = process.env.BOT_AGE || new Date(2020, 10, 27);
+
+exports.myDialogflowFulfillment = async (req, res) => {
+  console.log('My request body:', JSON.stringify(req.body, null, 2));
+  const intentName = req.body.queryResult.intent.displayName;
+
+  let answer;
+  switch (intentName) {
+    case '_YourAge':
+      const age = Number((new Date().getTime() - BOT_AGE.getTime()) / 31536000000).toFixed(0);
+      const warnText = 'You\'re quite bold to ask a lady her age.';
+      const ageText = `But sure, I'm ${age} years old.`;
+      const smartText = 'Quite smart for a human I dare to say.';
+      answer = { 'fulfillmentMessages': [{ 'text': { 'text': [warnText, ageText, smartText] } }] };
+      break;
+    case '_YourMaker':
+      const text = `This demonstration fulfillment has been made with love by ${authors}`;
+      answer = { 'fulfillmentMessages': [{ 'text': { 'text': [text] } }] };
+      break;
+    case '_ping':
+      answer = simpleResponse;
+      break;
+
+  }
+  res.send(answer);
+};
